da3ba9416f561f73b7fa39e75bf87a65edc5b80c
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2018-02-15 17:27:51 +0800

    Add files via upload

---
+++ myloanworldUI/app/controls/manage-contact-details-controller.js
@@ -0,0 +1,17 @@
+﻿//
+(function () {
+    angular.module('myapp').controller('manageContactDetailsController', function (authenticationService) {
+        var vm = this;
+        vm.loggedInUser = authenticationService.getLoggedInUser();
+        if (vm.loggedInUser.FeatureName == 'Admin') {
+            vm.getContactDetails = function () {
+                authenticationService.getContactDetails().then(function (resp) {
+                    vm.contactDetail = resp;
+                }, function (error) {
+                    console.log("Application Status fetching failed");
+                });
+            }
+        }
+        
+    })
+})();
