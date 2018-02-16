c785445cc9d754c54a645cd1e1056a50d5d95017
Ashok Tewatia <ashok.crediblesoftware@gmail.com>
2018-02-16 14:16:20 +0800

    Update footer-controller.js

--- myloanworldUI/app/controls/footer-controller.js
+++ myloanworldUI/app/controls/footer-controller.js
@@ -1,10 +1,16 @@
 (function () {
     angular.module('myapp').controller('footerController', function ($scope, commonService, applicationsService) {
-    var vm = this;
-    vm.footerLinks = commonService.footerLinks;
-    //on this page only static links are required but from service everything is returned
-    applicationsService.getApplicationType().then(function (resp) {
-        vm.footerLinks.col1 = resp;
-    }); 
-})
+        var vm = this;
+        vm.footerLinks = commonService.footerLinks;
+        //on this page only static links are required but from service everything is returned
+        applicationsService.getApplicationType().then(function (resp) {
+            vm.footerLinks.col1 = resp;
+        });
+
+        authenticationService.getContactDetails().then(function (resp) {
+            vm.contactDetail = resp[0];
+        }, function (error) {
+            console.log("contact detail fetch failed");
+        });
+    })
 })();
